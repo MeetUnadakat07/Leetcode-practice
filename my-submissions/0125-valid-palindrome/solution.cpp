0da27b06cfd231f8f30460358fae778185278f71
MeetUnadakat07 <meetunadakat06@gmail.com>
2025-04-17 14:26:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (55.24%)

---
+++ my-submissions/0125-valid-palindrome/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAlphaNumeric(char ch) {
+        if(ch>='0' && ch<='9' || tolower(ch)>='a' && tolower(ch)<='z') {
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string str) {
+        int st = 0, end = str.length()-1;
+        while(st<end) {
+            if(!isAlphaNumeric(str[st])) {
+                st++;
+                continue;
+            }
+            if(!isAlphaNumeric(str[end])) {
+                end--;
+                continue;
+            }
+            if(tolower(str[st]) != tolower(str[end])) {
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+};
