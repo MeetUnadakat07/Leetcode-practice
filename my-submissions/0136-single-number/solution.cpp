82b8dcba022ad5ded5ff8da86f233e57fb704737
MeetUnadakat07 <meetunadakat06@gmail.com>
2025-06-20 12:21:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (58.42%)

--- my-submissions/0136-single-number/solution.cpp
+++ my-submissions/0136-single-number/solution.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-
-        for(int val : nums)
-        {
-            ans ^= val;
+        for(int i = 0; i < nums.size(); i++) {
+            ans ^= nums[i];
         }
-
         return ans;
     }
 };
