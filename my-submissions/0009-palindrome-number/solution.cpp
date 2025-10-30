e549362812a0f1e9cde3132a56aa921e5015894e
MeetUnadakat07 <meetunadakat06@gmail.com>
2025-10-30 13:58:03 +0000

    Sync LeetCode submission Runtime - 1 ms (57.94%), Memory - 8.5 MB (65.60%)

--- my-submissions/0009-palindrome-number/solution.cpp
+++ my-submissions/0009-palindrome-number/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        int n = x;
-        long int revNum = 0;
-        while(x>0)
-        {
-            int rem = x%10;
-            revNum = revNum*10+rem;
-            x/=10;
+        long int rev, cpy = x;
+        int rem;
+        while(x > 0) {
+            rem = x % 10;
+            rev = (rev * 10) + rem;
+            x /= 10;
         }
-        return revNum == n;
+        return cpy == rev;
     }
 };
