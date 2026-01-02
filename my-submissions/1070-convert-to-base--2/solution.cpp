fc9b1a3c48b30fb0ddab23951f86ec23c130e716
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-01-02 12:15:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (11.70%)

---
+++ my-submissions/1070-convert-to-base--2/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        if(n == 0) return "0";
+        string ans = "";
+        while(n != 0) {
+            int rem = n % -2;
+            n = n / -2;
+            if(rem < 0) {
+                rem = rem + 2;
+                n = n + 1;
+            }
+            ans = to_string(rem) + ans;
+        }
+        return ans;
+    }
+};
