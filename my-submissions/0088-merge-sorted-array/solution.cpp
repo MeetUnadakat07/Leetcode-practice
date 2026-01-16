3ea5d3b1cc8c0bf4c7262dea8ae68dfcead98607
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-01-16 10:58:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (9.44%)

--- my-submissions/0088-merge-sorted-array/solution.cpp
+++ my-submissions/0088-merge-sorted-array/solution.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void merge(vector<int>& A, int m, vector<int>& B, int n) {
-        int idx = m+n-1, i = m-1, j = n-1;
-        while(i>=0 && j>=0)
-        {
-            if(A[i] >= B[j])
-            {
-                A[idx--] = A[i--];
-            }
-            else
-            {
-                A[idx--] = B[j--];
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> result;
+        int p1 = 0, p2 = 0;
+        for(int i = 0; i < m + n; i++) {
+            int val1 = (p1 == m) ? INT_MAX : nums1[p1];
+            int val2 = (p2 == n) ? INT_MAX : nums2[p2];
+            if(val1 <= val2) {
+                result.push_back(val1);
+                p1++;
+            } else {
+                result.push_back(val2);
+                p2++;
             }
         }
-        while(j >= 0)
-        {
-            A[idx--] = B[j--];
+        for(int i = 0; i < m + n; i++) {
+            nums1[i] = result[i];
         }
-        return;
     }
 };
