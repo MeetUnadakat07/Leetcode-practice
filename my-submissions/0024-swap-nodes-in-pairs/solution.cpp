847472390563fb0d6d4feeb595988a55ee339ef4
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-01-27 12:43:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.3 MB (19.88%)

---
+++ my-submissions/0024-swap-nodes-in-pairs/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head -> next == NULL) return head;
+        
+        ListNode *prev = head;
+        ListNode *curr = head -> next;
+
+        while(prev != NULL && curr != NULL) {
+            swap(curr -> val, prev -> val);
+            prev = curr -> next;
+            if(prev == NULL) break;
+            curr = prev -> next;
+        }
+        return head;
+    }
+};
