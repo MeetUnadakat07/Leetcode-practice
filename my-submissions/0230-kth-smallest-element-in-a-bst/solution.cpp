c1e92e796b99e290efb3e799d466e245f764d13b
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-04-07 17:10:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24.3 MB (90.16%)

--- my-submissions/0230-kth-smallest-element-in-a-bst/solution.cpp
+++ my-submissions/0230-kth-smallest-element-in-a-bst/solution.cpp
@@ -11,20 +11,22 @@
  */
 class Solution {
 public:
-    int search(TreeNode *root, int &k) {
-        if(root == NULL) return -1;
+    void helper(TreeNode *root, int k, int &count, int &ans) {
+        if(root == NULL) return;
 
-        int leftVal = search(root -> left, k);
-        if(leftVal != -1) return leftVal;
-
-        k--;
-
-        if(k == 0) return root -> val;
-
-        return search(root -> right, k);
+        helper(root -> left, k, count, ans);
+        count++;
+        if(count == k) {
+            ans = root -> val;
+            return;
+        }
+        helper(root -> right, k, count, ans);
     }
 
     int kthSmallest(TreeNode* root, int k) {
-        return search(root, k);
+        int count = 0;
+        int ans = -1;
+        helper(root, k, count, ans);
+        return ans;
     }
 };
