fae287a5c8c2f8e7f6d5fabeb95be4d72341cca3
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-05-15 11:12:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.2 MB (47.22%)

--- my-submissions/0153-find-minimum-in-rotated-sorted-array/solution.cpp
+++ my-submissions/0153-find-minimum-in-rotated-sorted-array/solution.cpp
@@ -5,9 +5,9 @@ public:
         int end = nums.size() - 1;
         while(st < end) {
             int mid = st + (end - st) / 2;
-            if(nums[mid] > nums[end]) {
+            if(nums[mid] > nums[end]) {     // Min is in the right half
                 st = mid + 1;
-            } else {
+            } else {                        // Min is in the left half (including the mid)
                 end = mid;
             }
         }
