aacd5e4e83536cd4e63863846dbfeee1f9d06be4
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-05-27 09:33:33 +0000

    Sync LeetCode submission Runtime - 3 ms (46.30%), Memory - 9.8 MB (49.64%)

--- my-submissions/0242-valid-anagram/solution.cpp
+++ my-submissions/0242-valid-anagram/solution.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
-        unordered_map<char, int> m;
+        unordered_map<char, int>m;
         for(int i = 0; i < s.size(); i++) {
-            if(m.count(s[i])) {
-                m[s[i]]++;
-            } else {
+            if(m.count(s[i]) == 0) {
                 m[s[i]] = 1;
+            } else {
+                m[s[i]]++;
             }
         }
         for(int i = 0; i < t.size(); i++) {
-            if(m.count(t[i])) {
-                if(m[t[i]] == 1) {
-                    m.erase(t[i]);
-                } else {
-                    m[t[i]]--;
-                }
-            } else {
-                return false;
-            }
+            m[t[i]]--;
+            if(m[t[i]] < 0) return false;
         }
-        if(m.size() == 0) return true;
-        return false;
+        for(pair<char, int> p : m) {
+            if(p.second != 0) return false;
+        }
+        return true;
     }
 };
