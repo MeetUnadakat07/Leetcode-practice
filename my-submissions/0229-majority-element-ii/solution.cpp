383a6dd414b79b94288e3e953e11f7043f170599
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-05-27 09:22:41 +0000

    Sync LeetCode submission Runtime - 13 ms (10.77%), Memory - 26.3 MB (35.27%)

--- my-submissions/0229-majority-element-ii/solution.cpp
+++ my-submissions/0229-majority-element-ii/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int, int> m;
         vector<int> ans;
         for(int i = 0; i < nums.size(); i++) {
@@ -10,10 +11,9 @@ public:
                 m[nums[i]] = 1;
             }
         }
-        
-        for(pair<int, int> n : m) {
-            if(n.second > (nums.size() / 3)) {
-                ans.push_back(n.first);
+        for(pair<int, int> p : m) {
+            if(p.second > n / 3) {
+                ans.push_back(p.first);
             }
         }
         return ans;
