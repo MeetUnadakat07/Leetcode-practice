4137c027ef2b72dd62b1f82715abf9658762c686
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-06-07 13:14:22 +0000

    Sync LeetCode submission Runtime - 43 ms (58.78%), Memory - 45.4 MB (71.11%)

--- my-submissions/0560-subarray-sum-equals-k/solution.cpp
+++ my-submissions/0560-subarray-sum-equals-k/solution.cpp
@@ -3,23 +3,18 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> m;  // sum, count
         m[0] = 1;
+        int sum = 0;
+        int ans = 0;
 
-        int currSum = 0;
-        int count = 0;
-
-        for(int i = 0; i < nums.size(); i++) {
-            currSum += nums[i];
-
-            if(m.count(currSum - k)) {
-                count += m[currSum - k];
+        for(int j = 0; j < nums.size(); j++) {
+            sum += nums[j];
+            // agar mil gaya to frequency me add karo (sum - k == 0)
+            if(m.count(sum - k)) {
+                ans += m[sum - k];
             }
-
-            if(m.count(currSum)) {
-                m[currSum]++;
-            } else {
-                m[currSum] = 1;
-            }
+            // warna us sum ke liye frequency add kar do (agar nahi hai to 1 ho jayega)
+            m[sum]++;
         }
-        return count;
+        return ans;
     }
 };
