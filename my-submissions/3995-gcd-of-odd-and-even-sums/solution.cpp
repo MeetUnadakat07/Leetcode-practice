53448e10a74a13962e765ce21b4241c54dd36cde
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-07-15 12:02:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (85.03%)

--- my-submissions/3995-gcd-of-odd-and-even-sums/solution.cpp
+++ my-submissions/3995-gcd-of-odd-and-even-sums/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOfOdd = pow(n, 2);
-        int sumOfEven = n * (n + 1);
-        return gcd(sumOfOdd, sumOfEven);
+        int oddSum = n * n;
+        int evenSum = n * (n + 1);
+        return gcd(evenSum, oddSum);
     }
 };
