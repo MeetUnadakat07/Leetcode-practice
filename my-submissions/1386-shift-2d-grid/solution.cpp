602777f27ce27de1a7a98112daab65e46cb84c38
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-07-20 14:51:04 +0000

    Sync LeetCode submission Runtime - 11 ms (13.07%), Memory - 18.2 MB (39.20%)

--- my-submissions/1386-shift-2d-grid/solution.cpp
+++ my-submissions/1386-shift-2d-grid/solution.cpp
@@ -3,12 +3,14 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int n = grid.size();
         int m = grid[0].size();
-        for(int i = 0; i < k; i++) {
+        k %= (m * n);
+
+        while(k--) {
             int prev = grid[n - 1][m - 1];
 
-            for(int row = 0; row < n; row++) {
-                for(int col = 0; col < m; col++) {
-                    swap(prev, grid[row][col]);
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                    swap(prev, grid[i][j]);
                 }
             }
         }
