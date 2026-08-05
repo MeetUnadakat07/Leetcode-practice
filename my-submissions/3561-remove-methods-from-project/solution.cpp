9f49018906c06eb6510904e2aa5859fab60250d3
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-08-05 15:37:15 +0000

    Sync LeetCode submission Runtime - 451 ms (24.15%), Memory - 370.7 MB (16.67%)

---
+++ my-submissions/3561-remove-methods-from-project/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void dfs(int u, vector<vector<int>> &adj, vector<bool> &vis) {
+        vis[u] = true;
+
+        for(int v : adj[u]) {
+            if(!vis[v]) {
+                dfs(v, adj, vis);
+            }
+        }
+    }
+
+    vector<int> remainingMethods(int n, int k,
+                                 vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+
+        for (auto e : invocations) {
+            adj[e[0]].push_back(e[1]);
+        }
+
+        vector<bool> suspicious(n, false);
+        dfs(k, adj, suspicious);
+
+        for (auto e : invocations) {
+            if (!suspicious[e[0]] && suspicious[e[1]]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++) {
+                    ans.push_back(i);
+                }
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
