1bd64750ea0b1f788879bc8b03cc767c9907fa94
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-08-19 18:04:54 +0000

    Sync LeetCode submission Runtime - 28 ms (87.41%), Memory - 10.7 MB (82.35%)

--- my-submissions/0044-wildcard-matching/solution.cpp
+++ my-submissions/0044-wildcard-matching/solution.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    bool isMatch(string t, string p) {
-        int n = t.size();
-        int m = p.size();
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        // vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
-        vector<vector<bool>> dp(n + 1, vector<bool> (m + 1, false));
-        dp[0][0] = true;
-
-        for(int j = 1; j <= m; j++) {   // initialize 0th row -> tect = ""
+        curr[0] = true;
+        for(int j = 1; j <= m; j++) {
             if(p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 1];
+                curr[j] = true;
+            } else {
+                break;
             }
         }
-
-        // 0th col -> pattern = "" (all will be false only)
+        prev = curr;
 
         for(int i = 1; i <= n; i++) {
+            curr[0] = 0;
             for(int j = 1; j <= m; j++) {
-                if(t[i - 1] == p[j - 1] || p[j - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
+                if(s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    curr[j] = prev[j - 1];
                 } else if(p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    curr[j] = prev[j] || curr[j - 1];
                 } else {
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }
             }
+            prev = curr;
         }
-
-        // for(int i = 0; i <= n; i++) {
-        //     for(int j = 0; j <= m; j++) {
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-
-        return dp[n][m];
+        return prev[m];
     }
 };
