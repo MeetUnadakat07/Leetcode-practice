a1a5667e9e9d8442c15117c5a441fc119349f847
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-08-22 09:09:34 +0000

    Sync LeetCode submission Runtime - 48 ms (72.41%), Memory - 79.3 MB (80.21%)

--- my-submissions/0123-best-time-to-buy-and-sell-stock-iii/solution.cpp
+++ my-submissions/0123-best-time-to-buy-and-sell-stock-iii/solution.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int rec(vector<int> &prices, int idx, int buy, int k, vector<vector<vector<int>>> &dp) {
-        if(idx == prices.size() || k == 0) return 0;
-
-        if(dp[idx][buy][k] != -1) {
-            return dp[idx][buy][k];
-        }
-
-        int ans = 0;
-
-        // you can buy
-        if(buy == 1) {
-            int buy = -prices[idx] + rec(prices, idx + 1, 0, k, dp);
-            int notBuy = rec(prices, idx + 1, 1, k, dp); 
-            ans += max(buy, notBuy);
-        } else {
-            int sell = prices[idx] + rec(prices, idx + 1, 1, k - 1, dp);
-            int notSell = rec(prices, idx + 1, 0, k, dp);
-            ans += max(sell, notSell);
-        }
-        return dp[idx][buy][k] = ans;
-    }
-
     int maxProfit(vector<int>& prices) {
         int k = 2, n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (k + 1, -1)));
-        return rec(prices, 0, 1, k, dp);
+        vector<vector<int>> after (2, vector<int> (k + 1, 0));
+        vector<vector<int>> curr (2, vector<int> (k + 1, 0));
+
+        for(int idx = n - 1; idx >= 0; idx--) {
+            for(int buy = 0; buy < 2; buy++) {
+                // for cap 0 answer will be 0 so we do not need to take that
+                for(int cap = 1; cap <= k; cap++) {
+                    if(buy == 1) {
+                        int take = -prices[idx] + after[0][cap];
+                        int notTake = after[1][cap]; 
+                        curr[buy][cap] = max(take, notTake);
+                    } else {
+                        int sell = prices[idx] + after[1][cap - 1];
+                        int notSell = after[0][cap];
+                        curr[buy][cap] = max(sell, notSell);
+                    }      
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
     }
 };
