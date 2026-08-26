22fce04ed77e74d81736218a754821bbce84625f
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-08-26 15:56:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.1 MB (86.37%)

--- my-submissions/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ my-submissions/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int rec(vector<int> &prices, int idx, int buy, vector<vector<int>> &dp) {
-        if(idx >= prices.size()) return 0;
-
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-
-        if(buy == 1) {
-            int take = -prices[idx] + rec(prices, idx + 1, 0, dp);
-            int notTake = rec(prices, idx + 1, 1, dp);
-            return dp[idx][buy] = max(take, notTake);
-        } else {
-            int sell = prices[idx] + rec(prices, idx + 2, 1, dp);
-            int notSell = rec(prices, idx + 1, 0, dp);
-            return dp[idx][buy] = max(sell, notSell);
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, -1));
-        return rec(prices, 0, 1, dp);
+        // vector<vector<int>> dp(n + 2, vector<int> (2, 0));
+        vector<int> curr(2, 0), next(2, 0);
+        int nextBuy = 0;        // dp[idx + 2][1]
+
+        for(int idx = n - 1; idx >= 0; idx--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                if(buy == 1) {
+                    int take = -prices[idx] + next[0];
+                    int notTake = next[1];
+                    curr[buy] = max(take, notTake);
+                } else {
+                    int sell = prices[idx] + nextBuy;
+                    int notSell = next[0];
+                    curr[buy] = max(sell, notSell);
+                }
+            }
+            nextBuy = next[1];
+            next = curr;
+        }
+        return curr[1];
     }
 };
