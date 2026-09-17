ca6765b8580cd742483800ca19cf01f4c408ab26
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-09-17 10:59:58 +0000

    Sync LeetCode submission Runtime - 15 ms (31.36%), Memory - 11.1 MB (33.09%)

--- my-submissions/1460-number-of-substrings-containing-all-three-characters/solution.cpp
+++ my-submissions/1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int st = 0, end = 0;
-        int count = 0;
-        int freq[3] = {0, 0, 0};
+        int st = 0, count = 0;
+        int n = s.size();
+        int freq[] = {0, 0, 0};
 
-        while(end < s.size()) {
+        for(int end = 0; end < n; end++) {
             freq[s[end] - 'a']++;
-
             while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-                count += s.size() - end;
+                count += n - end;
                 freq[s[st] - 'a']--;
                 st++;
             }
-            end++;
         }
         return count;
     }
