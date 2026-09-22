f459a4c99fe21c16270044a835ce6e09accddb3a
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-09-22 14:40:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (86.67%)

--- my-submissions/0152-maximum-product-subarray/solution.cpp
+++ my-submissions/0152-maximum-product-subarray/solution.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    pair<int, int> rec(vector<int> &nums, int i, int &ans) {
-        if(i == 0) {
-            ans = nums[0];    
-            return {nums[0], nums[0]};
-        }
-
-        auto [prevMax, prevMin] = rec(nums, i - 1, ans);
-
-        int currMax = max({nums[i], nums[i] * prevMax, nums[i] * prevMin});
-        int currMin = min({nums[i], nums[i] * prevMax, nums[i] * prevMin});
-
-        ans = max(currMax, ans);
-        return {currMax, currMin};
-    }
-
     int maxProduct(vector<int>& nums) {
-        int ans;
-        auto [mx, mn] = rec(nums, nums.size() - 1, ans);
+        int currMax = nums[0], currMin = nums[0], ans = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+            int tempMax = currMax;
+            currMax = max({nums[i], tempMax * nums[i], currMin * nums[i]});
+            currMin = min({nums[i], tempMax * nums[i], currMin * nums[i]});
+            ans = max(ans, currMax);
+        }
         return ans;
     }
 };
