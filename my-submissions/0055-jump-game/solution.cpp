f2d34d3dfbbbc7ee1d79cbcc3773610d52bfed58
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-09-23 15:26:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 52.2 MB (83.05%)

--- my-submissions/0055-jump-game/solution.cpp
+++ my-submissions/0055-jump-game/solution.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxIdx = 0;
+        int farthest = 0;
         for(int i = 0; i < nums.size(); i++) {
-            if(i <= maxIdx) {
-                maxIdx = max(maxIdx, i + nums[i]);
-            } else {
+            if(i > farthest) {
                 return false;
+            }
+
+            farthest = max(farthest, nums[i] + i);
+
+            if(farthest >= nums.size() - 1) {
+                return true;
             }
         }
         return true;
